740eded1455fb70c0d3cb69997dfe5b58c8d9255
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-08-21 16:24:01 -0300

    feat: create a tweet

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SignUpUserDTO } from './dto/user.dto';
+import { CreateTweetDTO } from './dto/tweet.dto';
 
 @Controller()
 export class AppController {
@@ -20,4 +21,12 @@ export class AppController {
       throw new HttpException(error.message, error.status);
     }
   }  
+  @Post('tweets')
+  createTweet(@Body() body: CreateTweetDTO) {
+    try {
+      return this.appService.createTweet(body);
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
+import { Tweet } from './entities/tweet.entity';
 import { SignUpUserDTO } from './dto/user.dto';
+import { CreateTweetDTO } from './dto/tweet.dto';
 
 @Injectable()
 export class AppService {
   private users: User[];
+  private tweets: Tweet[];
 
   constructor() {
     this.users = [];
+    this.tweets = [];
+ 
   }
 
   getHealth(): string {
@@ -19,5 +24,13 @@ export class AppService {
     return this.users.push(user);
   }
 
+  createTweet(body: CreateTweetDTO) {
+    const user: User = this.users.find(user => user.username === body.username);
+    if(!user) {
+      throw new HttpException('User not found!', 401);
+    }
 
+    const tweet = new Tweet(user, body.tweet);
+    return this.tweets.push(tweet);
+  }
 }
